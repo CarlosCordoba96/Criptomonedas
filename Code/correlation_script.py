decdcd54f59d5796fd5b7e4cfd14f8d4e302fe2c
itsquall <sergioacaraballo@gmail.com>
2017-12-17 19:21:30 +0100

    Generate better heatmap for correlations

--- Code/correlation_script.py
+++ Code/correlation_script.py
@@ -1,11 +1,16 @@
 import re
+import numpy as np
 import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
 
+from matplotlib import cm as cm
 from os import listdir
 from os.path import isfile, join
 
 
 cc_names = [re.sub('.csv', '', f) for f in listdir('Top100/') if isfile(join('Top100/', f))]
+cc_names.sort()
 
 cc_df = pd.DataFrame()
 
@@ -26,4 +31,20 @@ for currency in cc_names:
     # Add to principal crypto df
     cc_df = pd.concat([cc_df, df], axis=1, join_axes=[df.index])
 
-cc_df.corr().to_csv("crypcorrelation.csv")
+cc_df.sort_index(inplace=True)
+
+# cc_df = cc_df['2017-06-07':'2017-09-22']
+correlations = cc_df.corr(method='pearson')
+# correlations.to_csv("crypcorrelation.csv")
+
+# Create a mask to display only lower triangle
+mask = np.zeros_like(correlations)
+mask[np.triu_indices_from(mask)] = True
+
+# Create a heatmap with the mask
+sb.heatmap(correlations, cmap='RdYlGn_r', vmax=1.0, vmin=-1.0 , mask = mask, linewidths=2.5)
+
+# Show the plot
+plt.yticks(rotation=0, size=6)
+plt.xticks(rotation=90, size=6)
+plt.show()
